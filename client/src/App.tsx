45857a4b9794120f0487405e2a3e0de90235134c
aabongwa-lt <aabongwa@leantechniques.com>
2026-06-10 17:05:49 -0500

    Frontend changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {Link} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-     <main className={"p-20 h-full bg-red-400"}>
+     <main className={"p-20 h-screen "}>
          
-         <div className={"w-full h-full bg-slate-200 border border-gray-200"}>
-             <h1>Hello World!</h1>
+         <div className={"w-full h-full bg-slate-800 rounded-lg outline-2 outline-slate-700 p-5"}>
+             <h1 className={"text-slate-100"}>Hello World!</h1>
+             <nav className={"flex items-center justify-center"}>
+                 <li><Link to="/" className="w-full decoration-none bg-slate-600 rounded-lg py-2 hover:slate-600/80 hover:scale-103">Birds Listing</Link></li>
+             </nav>
+             
          </div>
      </main>
     </>
