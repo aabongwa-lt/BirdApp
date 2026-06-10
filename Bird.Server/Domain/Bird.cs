6953b3c3f73e20dfac441f7d973b7f32c7172344
aabongwa-lt <aabongwa@leantechniques.com>
2026-06-10 12:09:04 -0500

    Add .gitignore and remove build artifacts from tracking

--- Bird.Server/Domain/Bird.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Bird.Server.Domain;
-
-public class Bird
-{
-    public int Id { get; init; }
-    public string? Name { get; init; } = String.Empty;
-    public string? Description { get; init; } =  String.Empty;
-}
